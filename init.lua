b24283707073b2ef442942b655a802718c91b5a1
Eric <eric.so.dev@gmail.com>
2023-09-18 23:51:56 -0400

    init

--- init.lua
+++ init.lua
@@ -86,7 +86,7 @@ require('lazy').setup({
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
+      { 'j-hui/fidget.nvim',       tag = 'legacy', opts = {} },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
@@ -110,7 +110,7 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim', opts = {} },
+  { 'folke/which-key.nvim',  opts = {} },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
@@ -124,7 +124,8 @@ require('lazy').setup({
         changedelete = { text = '~' },
       },
       on_attach = function(bufnr)
-        vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk, { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
+        vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk,
+          { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
         vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
         vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
       end,
@@ -201,7 +202,7 @@ require('lazy').setup({
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
+  require 'kickstart.plugins.autoformat',
   -- require 'kickstart.plugins.debug',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
@@ -210,7 +211,7 @@ require('lazy').setup({
   --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {})
 
 -- [[ Setting options ]]
@@ -221,7 +222,7 @@ require('lazy').setup({
 vim.o.hlsearch = false
 
 -- Make line numbers default
-vim.wo.number = true
+vim.wo.relativenumber = true
 
 -- Enable mouse mode
 vim.o.mouse = 'a'
@@ -317,6 +318,10 @@ require('nvim-treesitter.configs').setup {
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
+  autotag = {
+    enable = true,
+    enable_rename = true,
+  },
 
   highlight = { enable = true },
   indent = { enable = true },
@@ -365,12 +370,12 @@ require('nvim-treesitter.configs').setup {
     },
     swap = {
       enable = true,
-      swap_next = {
-        ['<leader>a'] = '@parameter.inner',
-      },
-      swap_previous = {
-        ['<leader>A'] = '@parameter.inner',
-      },
+      --swap_next = {
+      --  ['<leader>a'] = '@parameter.inner',
+      --},
+      --swap_previous = {
+      --  ['<leader>A'] = '@parameter.inner',
+      --},
     },
   },
 }
---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,22 @@
+return {
+  "ThePrimeagen/harpoon",
+  -- Optional dependency
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local mark = require("harpoon.mark");
+    local ui = require("harpoon.ui");
+
+    vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "add harpoon mark" })
+    vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
+    vim.keymap.set("n", "<leader>ha", function() ui.nav_file(1) end, { desc = "go to harpoon mark (1)" })
+    vim.keymap.set("n", "<leader>hb", function() ui.nav_file(2) end, { desc = "go to harpoon mark (2)" })
+    vim.keymap.set("n", "<leader>hc", function() ui.nav_file(3) end, { desc = "go to harpoon mark (3)" })
+    vim.keymap.set("n", "<leader>hd", function() ui.nav_file(4) end, { desc = "go to harpoon mark (4)" })
+
+    vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+    vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
+    vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
+    vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+  end,
+}
