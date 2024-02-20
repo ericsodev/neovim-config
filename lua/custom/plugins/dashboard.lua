fef2d1d334eb4312e45970ac1533e7839e918c28
Eric <eric.so.dev@gmail.com>
2024-02-20 04:28:12 -0500

    Update indent blankline

--- lua/custom/plugins/dashboard.lua
+++ lua/custom/plugins/dashboard.lua
@@ -17,7 +17,6 @@ return {
   'nvimdev/dashboard-nvim',
   event = 'VimEnter',
   config = function()
-    require("indent_blankline.commands").disable()
     require('dashboard').setup {
       -- config
       theme = "hyper",
