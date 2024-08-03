ad60cbc4963d52a69b5e50bea0bf8d6cb47c8b5b
Eric <eric.so.dev@gmail.com>
2024-08-03 19:24:29 -0400

    feat: setup tailwind, none ls init

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -18,6 +18,54 @@ return { -- LSP Configuration & Plugins
 		end
 		vim.keymap.set("n", "<leader>li", toggleInlayHints, { desc = "Toggle [L]SP [I]nlay hints" })
 	end,
+	opts = {
+		servers = {
+			svelte = {
+				capabilities = {
+					workspace = {
+						didChangeWatchedFiles = vim.fn.has("nvim-0.10") == 0 and { dynamicRegistration = true },
+					},
+				},
+			},
+			tailwindcss = {
+				-- exclude a filetype from the default_config
+				filetypes_exclude = { "markdown" },
+				-- add additional filetypes to the default_config
+				filetypes_include = {},
+				-- to fully override the default_config, change the below
+				-- filetypes = {}
+			},
+		},
+		setup = {
+			tailwindcss = function(_, opts)
+				local tw = require("lspconfig.server_configurations.tailwindcss")
+				opts.filetypes = opts.filetypes or {}
+
+				-- Add default filetypes
+				vim.list_extend(opts.filetypes, tw.default_config.filetypes)
+
+				-- Remove excluded filetypes
+				--- @param ft string
+				opts.filetypes = vim.tbl_filter(function(ft)
+					return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+				end, opts.filetypes)
+
+				-- Additional settings for Phoenix projects
+				opts.settings = {
+					tailwindCSS = {
+						includeLanguages = {
+							elixir = "html-eex",
+							eelixir = "html-eex",
+							heex = "html-eex",
+						},
+					},
+				}
+
+				-- Add additional filetypes
+				vim.list_extend(opts.filetypes, opts.filetypes_include or {})
+			end,
+		},
+	},
 	config = function()
 		-- Brief Aside: **What is LSP?**
 		--
