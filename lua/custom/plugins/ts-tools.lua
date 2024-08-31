59bede3dc4c41fd82ea46d4475e0aced338bab0a
Eric <eric.so.dev@gmail.com>
2024-08-30 23:48:59 -0400

    feat: tstools perf

--- lua/custom/plugins/ts-tools.lua
+++ lua/custom/plugins/ts-tools.lua
@@ -22,18 +22,12 @@ return {
 				-- spawn additional tsserver instance to calculate diagnostics on it
 				separate_diagnostic_server = true,
 				-- "change"|"insert_leave" determine when the client asks the server about diagnostic
-				publish_diagnostic_on = "change",
+				publish_diagnostic_on = "insert_leave",
 				-- array of strings("fix_all"|"add_missing_imports"|"remove_unused"|
 				-- "remove_unused_imports"|"organize_imports") -- or string "all"
 				-- to include all supported code actions
 				-- specify commands exposed as code_actions
-				expose_as_code_action = {
-					"fix_all",
-					"add_missing_imports",
-					"remove_unused",
-					"remove_unused_imports",
-					"organize_imports",
-				},
+				expose_as_code_action = "all",
 				-- string|nil - specify a custom path to `tsserver.js` file, if this is nil or file under path
 				-- not exists then standard path resolution strategy is applied
 				tsserver_path = nil,
@@ -46,6 +40,9 @@ return {
 				-- described below
 				tsserver_format_options = {},
 				tsserver_file_preferences = {
+					importModuleSpecifierEnding = "minimal",
+					allowRenameOfImportPath = true,
+					includeCompletionsForImportStatements = true,
 					includeInlayParameterNameHints = "all",
 					includeCompletionsForModuleExports = false,
 				},
