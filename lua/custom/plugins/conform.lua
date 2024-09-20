a0d8511885183a49473e31f275d1f210716ee096
Eric So <eric.so@tetratrust.com>
2024-09-20 09:06:34 -0400

    feat: update conform opts format

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -4,7 +4,7 @@ return { -- Autoformat
 		notify_on_error = false,
 		format_on_save = {
 			timeout_ms = 500,
-			lsp_fallback = false,
+			lsp_fallback = true,
 		},
 		formatters_by_ft = {
 			lua = { "stylua" },
@@ -13,13 +13,13 @@ return { -- Autoformat
 			--
 			-- You can use a sub-list to tell conform to run *until* a formatter
 			-- is found.
-			javascript = { { "prettierd", "prettier" } },
-			typescriptreact = { { "prettierd", "prettier" } },
-			javascriptreact = { { "prettierd", "prettier" } },
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+			typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+			javascriptreact = { "prettierd", "prettier", stop_after_first = true },
 			html = { "prettier" },
 			css = { "prettier" },
 			scss = { "prettier" },
-			typescript = { "prettierd" },
+			typescript = { "prettierd", stop_after_first = true },
 			svelte = { "prettierd" },
 		},
 	},
