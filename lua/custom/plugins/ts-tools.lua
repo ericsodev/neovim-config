3e36298a607c7c8471b5d9063ee33ada81de27ea
Eric So <eric.so@tetratrust.com>
2024-10-13 14:45:21 -0400

    feat: use mason tsserver for ts tools provider

--- lua/custom/plugins/ts-tools.lua
+++ lua/custom/plugins/ts-tools.lua
@@ -1,72 +1,73 @@
 return {
-	"pmizio/typescript-tools.nvim",
-	dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-	opts = {},
-	config = function()
-		require("typescript-tools").setup({
-			on_attach = function(client, bufnr)
-				vim.keymap.set(
-					{ "n", "v" },
-					"<leader>ca",
-					vim.lsp.buf.code_action,
-					{ buffer = bufnr, desc = "TS Code [A]ctions" }
-				)
-				local function toggleInlayHints()
-					if client.server_capabilities.inlayHintProvider then
-						vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-					end
-				end
-				vim.keymap.set("n", "<leader>li", toggleInlayHints, { desc = "Toggle [L]SP [I]nlay hints" })
-			end,
-			settings = {
-				-- spawn additional tsserver instance to calculate diagnostics on it
-				separate_diagnostic_server = true,
-				-- "change"|"insert_leave" determine when the client asks the server about diagnostic
-				publish_diagnostic_on = "insert_leave",
-				-- array of strings("fix_all"|"add_missing_imports"|"remove_unused"|
-				-- "remove_unused_imports"|"organize_imports") -- or string "all"
-				-- to include all supported code actions
-				-- specify commands exposed as code_actions
-				expose_as_code_action = "all",
-				-- string|nil - specify a custom path to `tsserver.js` file, if this is nil or file under path
-				-- not exists then standard path resolution strategy is applied
-				tsserver_path = nil,
-				-- specify a list of plugins to load by tsserver, e.g., for support `styled-components`
-				-- (see 💅 `styled-components` support section)
-				tsserver_plugins = {},
-				-- this value is passed to: https://nodejs.org/api/cli.html#--max-old-space-sizesize-in-megabytes
-				-- memory limit in megabytes or "auto"(basically no limit)
-				tsserver_max_memory = "auto",
-				-- described below
-				tsserver_format_options = {},
-				tsserver_file_preferences = {
-					importModuleSpecifierEnding = "minimal",
-					allowRenameOfImportPath = true,
-					includeCompletionsForImportStatements = true,
-					includeInlayParameterNameHints = "all",
-					includeCompletionsForModuleExports = false,
-				},
-				-- locale of all tsserver messages, supported locales you can find here:
-				-- https://github.com/microsoft/TypeScript/blob/3c221fc086be52b19801f6e8d82596d04607ede6/src/compiler/utilitiesPublic.ts#L620
-				tsserver_locale = "en",
-				-- mirror of VSCode's `typescript.suggest.completeFunctionCalls`
-				complete_function_calls = false,
-				include_completions_with_insert_text = true,
-				-- CodeLens
-				-- WARNING: Experimental feature also in VSCode, because it might hit performance of server.
-				-- possible values: ("off"|"all"|"implementations_only"|"references_only")
-				code_lens = "off",
-				-- by default code lenses are displayed on all referencable values and for some of you it can
-				-- be too much this option reduce count of them by removing member references from lenses
-				disable_member_code_lens = true,
-				-- JSXCloseTag
-				-- WARNING: it is disabled by default (maybe you configuration or distro already uses nvim-ts-autotag,
-				-- that maybe have a conflict if enable this feature. )
-				jsx_close_tag = {
-					enable = false,
-					filetypes = { "javascriptreact", "typescriptreact" },
-				},
-			},
-		})
-	end,
+  'pmizio/typescript-tools.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+  opts = {},
+  config = function()
+    -- Keep tsserver installed without auto attaching
+    local lspconfig = require 'lspconfig'
+    lspconfig.tsserver.setup {
+      autostart = false,
+    }
+
+    require('typescript-tools').setup {
+      on_attach = function(client, bufnr)
+        vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, { buffer = bufnr, desc = 'TS Code [A]ctions' })
+        local function toggleInlayHints()
+          if client.server_capabilities.inlayHintProvider then
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+          end
+        end
+        vim.keymap.set('n', '<leader>li', toggleInlayHints, { desc = 'Toggle [L]SP [I]nlay hints' })
+      end,
+      settings = {
+        -- spawn additional tsserver instance to calculate diagnostics on it
+        separate_diagnostic_server = true,
+        -- "change"|"insert_leave" determine when the client asks the server about diagnostic
+        publish_diagnostic_on = 'insert_leave',
+        -- array of strings("fix_all"|"add_missing_imports"|"remove_unused"|
+        -- "remove_unused_imports"|"organize_imports") -- or string "all"
+        -- to include all supported code actions
+        -- specify commands exposed as code_actions
+        expose_as_code_action = 'all',
+        -- string|nil - specify a custom path to `tsserver.js` file, if this is nil or file under path
+        -- not exists then standard path resolution strategy is applied
+        tsserver_path = nil,
+        -- specify a list of plugins to load by tsserver, e.g., for support `styled-components`
+        -- (see 💅 `styled-components` support section)
+        tsserver_plugins = {},
+        -- this value is passed to: https://nodejs.org/api/cli.html#--max-old-space-sizesize-in-megabytes
+        -- memory limit in megabytes or "auto"(basically no limit)
+        tsserver_max_memory = 'auto',
+        -- described below
+        tsserver_format_options = {},
+        tsserver_file_preferences = {
+          importModuleSpecifierEnding = 'minimal',
+          allowRenameOfImportPath = true,
+          includeCompletionsForImportStatements = true,
+          includeInlayParameterNameHints = 'all',
+          includeCompletionsForModuleExports = false,
+        },
+        -- locale of all tsserver messages, supported locales you can find here:
+        -- https://github.com/microsoft/TypeScript/blob/3c221fc086be52b19801f6e8d82596d04607ede6/src/compiler/utilitiesPublic.ts#L620
+        tsserver_locale = 'en',
+        -- mirror of VSCode's `typescript.suggest.completeFunctionCalls`
+        complete_function_calls = false,
+        include_completions_with_insert_text = true,
+        -- CodeLens
+        -- WARNING: Experimental feature also in VSCode, because it might hit performance of server.
+        -- possible values: ("off"|"all"|"implementations_only"|"references_only")
+        code_lens = 'off',
+        -- by default code lenses are displayed on all referencable values and for some of you it can
+        -- be too much this option reduce count of them by removing member references from lenses
+        disable_member_code_lens = true,
+        -- JSXCloseTag
+        -- WARNING: it is disabled by default (maybe you configuration or distro already uses nvim-ts-autotag,
+        -- that maybe have a conflict if enable this feature. )
+        jsx_close_tag = {
+          enable = false,
+          filetypes = { 'javascriptreact', 'typescriptreact' },
+        },
+      },
+    }
+  end,
 }
