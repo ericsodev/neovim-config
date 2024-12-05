8baca615b9a8f830c60ea534ae74b51d484fb19e
Eric So <eric.so@tetratrust.com>
2024-12-05 15:44:02 -0500

    feat: back to 0

--- lua/custom/plugins/rosepine.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "rose-pine/neovim",
-  name = "rose-pine",
-  -- Optional dependency
-  config = function()
-    require("rose-pine").setup({ dark_variant = "main" })
-    vim.cmd("colorscheme rose-pine")
-  end,
-}
