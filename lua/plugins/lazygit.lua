3b11369eb2e4b98cb1c6c79ee30145fdd6800c6d
Eric So <eric.so@tetratrust.com>
2024-12-11 16:43:55 -0500

    feat: add lazygit

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -18,4 +18,10 @@ return {
   keys = {
     { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
   },
+  config = function()
+    vim.g.lazygit_on_exit_callback = function()
+      local state = require('neo-tree.sources.manager').get_state 'filesystem'
+      require('neo-tree.sources.filesystem.commands').refresh(state)
+    end
+  end,
 }
