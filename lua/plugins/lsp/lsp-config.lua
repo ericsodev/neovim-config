05a62569df8e8090154106a5019e4f7c1537902f
Eric So <eric.so@tetratrust.com>
2024-12-24 15:23:03 -0500

    feat: vstsls

--- lua/plugins/lsp/lsp-config.lua
+++ lua/plugins/lsp/lsp-config.lua
@@ -148,6 +148,7 @@ return {
       -- But for many setups, the LSP (`ts_ls`) will work just fine
       ts_ls = {
         ensure_installed = true,
+        enabled = false,
         filetypes = {
           'javascript',
           'javascriptreact',
@@ -159,10 +160,46 @@ return {
         },
 
         single_file_support = true,
-        enabled = true,
       },
       eslint = {},
-      --
+      vtsls = {
+        filetypes = {
+          'javascript',
+          'javascriptreact',
+          'javascript.jsx',
+          'typescript',
+          'typescriptreact',
+          'typescript.tsx',
+          'svelte',
+        },
+        settings = {
+          complete_function_calls = true,
+          vtsls = {
+            enableMoveToFileCodeAction = true,
+            autoUseWorkspaceTsdk = true,
+            experimental = {
+              maxInlayHintLength = 30,
+              completion = {
+                enableServerSideFuzzyMatch = true,
+              },
+            },
+          },
+          typescript = {
+            updateImportsOnFileMove = { enabled = 'always' },
+            suggest = {
+              completeFunctionCalls = true,
+            },
+            inlayHints = {
+              enumMemberValues = { enabled = true },
+              functionLikeReturnTypes = { enabled = true },
+              parameterNames = { enabled = 'literals' },
+              parameterTypes = { enabled = true },
+              propertyDeclarationTypes = { enabled = true },
+              variableTypes = { enabled = false },
+            },
+          },
+        },
+      },
 
       lua_ls = {
         -- cmd = {...},
