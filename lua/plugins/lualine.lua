c19ab1ce404c1e17198fc3760758c88586d64ded
Eric So <eric.so@tetratrust.com>
2024-12-24 15:22:45 -0500

    feat: minor qol tweaks

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -59,7 +59,7 @@ return {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
         lualine_c = { 'filename' },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype', 'location' },
         lualine_y = { diff, diagnostics },
         lualine_z = {},
       },
