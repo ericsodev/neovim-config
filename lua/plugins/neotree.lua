c42f8d0e57582ebaaec27f8b8d6d7931620a4ab7
Eric So <eric.so@tetratrust.com>
2024-12-24 15:21:48 -0500

    fix: still tryna fix neotree refresh

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -9,23 +9,29 @@ return {
   },
   config = function()
     require('neo-tree').setup {
+      enabled = true,
+      close_if_last_file = true,
       source_selector = {
         winbar = false,
         statusline = false,
       },
       filesystem = {
-        follow_current_file = { enabled = true },
+        follow_current_file = { enabled = true, leave_dirs_open = true },
+        bind_to_cwd = true,
+        use_libuv_file_watcher = true,
       },
+      sources = { 'filesystem', 'buffers', 'git_status' },
+      open_files_do_not_replace_types = { 'terminal', 'Trouble', 'trouble', 'qf', 'Outline' },
       git_status = {
         symbols = {
-          -- Change type
-          added = '', -- or "✚", but this is redundant info if you use git_status_colors on the name
-          modified = '', -- or "", but this is redundant info if you use git_status_colors on the name
-          deleted = '✖', -- this can only be used in the git_status source
-          renamed = '󰁕', -- this can only be used in the git_status source
-          -- Status type
-          untracked = '',
-          ignored = '',
+          -- -- Change type
+          -- added = '', -- or "✚", but this is redundant info if you use git_status_colors on the name
+          -- modified = '', -- or "", but this is redundant info if you use git_status_colors on the name
+          -- deleted = '✖', -- this can only be used in the git_status source
+          -- renamed = '󰁕', -- this can only be used in the git_status source
+          -- -- Status type
+          -- untracked = '',
+          -- ignored = '',
           unstaged = '󰄱',
           staged = '',
           conflict = '',
