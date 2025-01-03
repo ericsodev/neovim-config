ee57a5bd5b8235b6b8cebf41506c3dbf4b1d5c15
Eric <eric.so.dev@gmail.com>
2025-01-03 18:00:27 -0500

    chore: remove unused neotree and lazygit plugins

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -8,36 +8,36 @@ return {
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
-    require('neo-tree').setup {
-      enabled = true,
-      close_if_last_file = true,
-      source_selector = {
-        winbar = false,
-        statusline = false,
-      },
-      filesystem = {
-        follow_current_file = { enabled = true, leave_dirs_open = true },
-        bind_to_cwd = true,
-        use_libuv_file_watcher = true,
-      },
-      sources = { 'filesystem', 'buffers', 'git_status' },
-      open_files_do_not_replace_types = { 'terminal', 'Trouble', 'trouble', 'qf', 'Outline' },
-      git_status = {
-        symbols = {
-          -- -- Change type
-          -- added = '', -- or "✚", but this is redundant info if you use git_status_colors on the name
-          -- modified = '', -- or "", but this is redundant info if you use git_status_colors on the name
-          -- deleted = '✖', -- this can only be used in the git_status source
-          -- renamed = '󰁕', -- this can only be used in the git_status source
-          -- -- Status type
-          -- untracked = '',
-          -- ignored = '',
-          unstaged = '󰄱',
-          staged = '',
-          conflict = '',
-        },
-      },
-    }
-    vim.keymap.set('n', '<leader>t', ':Neotree toggle<cr>', { desc = '[T]oggle Neotree' })
+    -- require('neo-tree').setup {
+    --   enabled = false,
+    --   close_if_last_file = true,
+    --   source_selector = {
+    --     winbar = false,
+    --     statusline = false,
+    --   },
+    --   filesystem = {
+    --     follow_current_file = { enabled = true, leave_dirs_open = true },
+    --     bind_to_cwd = true,
+    --     use_libuv_file_watcher = true,
+    --   },
+    --   sources = { 'filesystem', 'buffers', 'git_status' },
+    --   open_files_do_not_replace_types = { 'terminal', 'Trouble', 'trouble', 'qf', 'Outline' },
+    --   git_status = {
+    --     symbols = {
+    --       -- -- Change type
+    --       -- added = '', -- or "✚", but this is redundant info if you use git_status_colors on the name
+    --       -- modified = '', -- or "", but this is redundant info if you use git_status_colors on the name
+    --       -- deleted = '✖', -- this can only be used in the git_status source
+    --       -- renamed = '󰁕', -- this can only be used in the git_status source
+    --       -- -- Status type
+    --       -- untracked = '',
+    --       -- ignored = '',
+    --       unstaged = '󰄱',
+    --       staged = '',
+    --       conflict = '',
+    --     },
+    --   },
+    -- }
+    -- vim.keymap.set('n', '<leader>t', ':Neotree toggle<cr>', { desc = '[T]oggle Neotree' })
   end,
 }
