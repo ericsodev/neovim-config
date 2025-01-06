0a99c05bfc82ebb784c5201bb3778266b448f431
Eric <eric.so.dev@gmail.com>
2025-01-05 22:47:03 -0500

    feat: harpoon vsplit and hsplit

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -8,9 +8,7 @@ return {
     -- REQUIRED
     harpoon:setup {
       settings = {
-
         save_on_toggle = true,
-
         sync_on_ui_close = true,
       },
     }
@@ -35,6 +33,18 @@ return {
     vim.keymap.set('n', '¢', function()
       harpoon:list():select(4)
     end)
+
+    harpoon:extend {
+      UI_CREATE = function(cx)
+        vim.keymap.set('n', '<C-v>', function()
+          harpoon.ui:select_menu_item { vsplit = true }
+        end, { buffer = cx.bufnr })
+
+        vim.keymap.set('n', '<C-h>', function()
+          harpoon.ui:select_menu_item { hsplit = true }
+        end, { buffer = cx.bufnr })
+      end,
+    }
 
     -- Toggle previous & next buffers stored within Harpoon list
     -- vim.keymap.set('n', '<C-S-P>', function()
