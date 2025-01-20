94f64ae2862048ef33283e829f10dd315a4d2466
Eric So <eric.so@tetratrust.com>
2025-01-20 08:54:18 -0500

    fix: terraform not loading lsp

--- lua/vim/autocommands.lua
+++ lua/vim/autocommands.lua
@@ -11,3 +11,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd('BufEnter', {
+  pattern = '*.tf,*.tfvars',
+  callback = function()
+    vim.opt_local.filetype = 'terraform'
+  end,
+})
