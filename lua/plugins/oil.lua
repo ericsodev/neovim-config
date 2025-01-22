42a3cac330bdedba12cc1e732991590aa9ae3227
Eric So <eric.so@tetratrust.com>
2025-01-22 11:57:20 -0500

    feat: oil winbar

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -6,7 +6,20 @@ return {
   -- Optional dependencies
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  --
   config = function()
+    -- Declare a global function to retrieve the current directory
+    function _G.get_oil_winbar()
+      local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+      local dir = require('oil').get_current_dir(bufnr)
+      if dir then
+        return vim.fn.fnamemodify(dir, ':~')
+      else
+        -- If there is no current directory (e.g. over ssh), just show the buffer name
+        return vim.api.nvim_buf_get_name(0)
+      end
+    end
+
     require('oil').setup {
       view_options = {
         show_hidden = true,
@@ -17,6 +30,9 @@ return {
         ['<C-h>'] = { 'actions.select', opts = { horizontal = true } },
       },
       watch_for_changes = true,
+      win_options = {
+        winbar = '%!v:lua.get_oil_winbar()',
+      },
     }
 
     -- Auto enable preview on select
@@ -30,6 +46,6 @@ return {
       end),
     })
 
-    vim.keymap.set('n', '_', require('oil').toggle_float)
+    vim.keymap.set('n', '_', '<CMD>Oil<Cr>')
   end,
 }
