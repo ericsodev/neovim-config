5bfd78949b52a137fd7d0043915dba050fac5a52
Eric So <eric.so@tetratrust.com>
2025-03-02 18:07:16 -0500

    feat: trying out new surround keybinds

--- lua/plugins/mini-surround.lua
+++ lua/plugins/mini-surround.lua
@@ -1,17 +1,19 @@
 return {
   'echasnovski/mini.surround',
-  opts = {
-    mappings = {
-      add = 'ma', -- Add surrounding in Normal and Visual modes
-      delete = 'md', -- Delete surrounding
-      find = 'mf', -- Find surrounding (to the right)
-      find_left = 'mF', -- Find surrounding (to the left)
-      highlight = 'mh', -- Highlight surrounding
-      replace = 'mr', -- Replace surrounding
-      update_n_lines = 'mn', -- Update `n_lines`
+  config = function()
+    require('mini.surround').setup {
+      mappings = {
+        add = '<C-m>a', -- Add surrounding in Normal and Visual modes
+        delete = '<C-m>d', -- Delete surrounding
+        find = '<C-m>f', -- Find surrounding (to the right)
+        find_left = '<C-m>F', -- Find surrounding (to the left)
+        highlight = '<C-m>h', -- Highlight surrounding
+        replace = '<C-m>r', -- Replace surrounding
+        update_n_lines = '<C-m>n', -- Update `n_lines`
 
-      suffix_last = 'l', -- Suffix to search with "prev" method
-      suffix_next = 'n', -- Suffix to search with "next" method
-    },
-  },
+        suffix_last = 'l', -- Suffix to search with "prev" method
+        suffix_next = 'n', -- Suffix to search with "next" method
+      },
+    }
+  end,
 }
